d440b879f89c9b5e7dbcb7f3a5f54c66737a3ed3
kwrona05 <kwrona05@gmail.com>
2024-10-11 09:08:02 +0200

    delete code from Days.jsx and use Form component

--- lessonPlan/src/components/Days.jsx
+++ lessonPlan/src/components/Days.jsx
@@ -1,46 +1,9 @@
-import { useState } from "react";
-const Days = ({ title }) => {
-  const [lessons, setLessons] = useState("");
-  const [lessonList, setLessonList] = useState([]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (lessons) {
-      setLessonList([...lessonList, lessons]);
-      setLessons("");
-    }
-  };
-
-  // const addTask = () => {
-  //   <form onSubmit={handleSubmit}>
-  //     <input
-  //       type="text"
-  //       value={lessons}
-  //       placeholder="Enter your lesson"
-  //       onChange={(e) => setLessons(e.target.value)}
-  //     />
-  //     <button type="submit">Submit</button>
-  //   </form>;
-  // };
-
+import Form from "./Form";
+const Days = ({ id, title }) => {
   return (
-    <div>
+    <div id={id}>
       <h2>{title}</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter your lesson"
-          value={lessons}
-          onChange={(e) => setLessons(e.target.value)}
-        />
-        <button type="submit">Add Lesson</button>
-      </form>
-
-      <ul>
-        {lessonList.map((lesson, index) => (
-          <li key={index}>{lesson}</li>
-        ))}
-      </ul>
+      <Form />
     </div>
   );
 };
