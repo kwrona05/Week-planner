761105269f41c63cb4bff790d2ba869ed514fcbb
kwrona05 <kwrona05@gmail.com>
2024-10-11 21:20:28 +0200

    add day div class name to div in Form.jsx

--- lessonPlan/src/components/Days.jsx
+++ lessonPlan/src/components/Days.jsx
@@ -1,7 +1,7 @@
 import Form from "./Form";
 const Days = ({ id, title }) => {
   return (
-    <div id={id}>
+    <div className="day-div" id={id}>
       <h2>{title}</h2>
       <Form />
     </div>
