a08e1e709c697f10eb24bf6f3698d23064d347de
kwrona05 <kwrona05@gmail.com>
2024-10-12 11:42:14 +0200

    add animation for each day tile

--- lessonPlan/src/components/Days.jsx
+++ lessonPlan/src/components/Days.jsx
@@ -1,7 +1,32 @@
 import Form from "./Form";
+import { useRef } from "react";
 const Days = ({ id, title }) => {
+  const tiltBox = useRef(null);
+  const handleMouseMove = (e) => {
+    const { offsetWidth: width, offsetHeight: height } = tiltBox.current;
+    const { clientX: x, clientY: y } = e;
+
+    const xPos = x / width - 0.5;
+    const yPos = y / height - 0.5;
+
+    const tiltX = yPos * 10;
+    const tiltY = xPos * 10;
+
+    tiltBox.current.style.transform = `rotateX(${tiltX}deg) rotateY(${tiltY}deg)`;
+  };
+
+  const handleMouseLeave = () => {
+    tiltBox.current.style.transform = "rotateX(0deg) rotateY(0deg)";
+  };
+
   return (
-    <div className="day-div" id={id}>
+    <div
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      ref={tiltBox}
+      className="day-div"
+      id={id}
+    >
       <h2>{title}</h2>
       <Form />
     </div>
